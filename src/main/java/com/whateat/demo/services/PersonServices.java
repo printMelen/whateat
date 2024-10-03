86fc479c722bdbcc8b515acab118434b8005fffd
printMelen <alvaro.redondofgb@gmail.com>
2024-10-04 01:31:06 +0200

    Añadiendo la estructura de las clases Person

---
+++ src/main/java/com/whateat/demo/services/PersonServices.java
@@ -0,0 +1,8 @@
+package com.whateat.demo.services;
+
+import com.whateat.demo.model.Person;
+
+public interface PersonServices {
+
+    public void createPerson(Person person);
+}
---
+++ src/main/java/com/whateat/demo/servicesImpl/PersonServicesImpl.java
@@ -0,0 +1,12 @@
+package com.whateat.demo.servicesImpl;
+
+import com.whateat.demo.model.Person;
+import com.whateat.demo.services.PersonServices;
+
+public class PersonServicesImpl implements PersonServices {
+
+    @Override
+    public void createPerson(Person person) {
+        throw new UnsupportedOperationException("Unimplemented method 'createPerson'");
+    }
+}
