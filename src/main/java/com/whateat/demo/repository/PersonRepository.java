3be5e40bf85926a3089162746ac6df4cff014616
printMelen <alvaro.redondofgb@gmail.com>
2024-10-04 00:39:32 +0200

    Añadiendo el repositorio de Person

---
+++ src/main/java/com/whateat/demo/repository/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.whateat.demo.repository;
+import com.whateat.demo.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Integer> {
+
+}
