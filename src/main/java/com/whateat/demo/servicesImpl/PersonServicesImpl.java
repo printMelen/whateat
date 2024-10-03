e5ee6e2c4fb21dbfe07f5dba7e768dcd4f01a2e5
printMelen <alvaro.redondofgb@gmail.com>
2024-10-04 01:55:14 +0200

    Conectando el repositorio a la implementacion por inyección en el constructor

--- src/main/java/com/whateat/demo/servicesImpl/PersonServicesImpl.java
+++ src/main/java/com/whateat/demo/servicesImpl/PersonServicesImpl.java
@@ -1,12 +1,23 @@
 package com.whateat.demo.servicesImpl;
 
+import org.springframework.stereotype.Service;
+
 import com.whateat.demo.model.Person;
+import com.whateat.demo.repository.PersonRepository;
 import com.whateat.demo.services.PersonServices;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 
 public class PersonServicesImpl implements PersonServices {
 
+    private final PersonRepository personRepository;
+
     @Override
     public void createPerson(Person person) {
-        throw new UnsupportedOperationException("Unimplemented method 'createPerson'");
+        personRepository.save(person);
+        //throw new UnsupportedOperationException("Unimplemented method 'createPerson'");
     }
 }
